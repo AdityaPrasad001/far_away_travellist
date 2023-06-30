820c5fd865543317b86a569a2d6b85af6869088b
Aditya <prasad.aditya000@gmail.com>
2023-06-30 23:13:09 +0530

    added sorting, clear list, stats details features

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,14 +3,8 @@ import Form from "./Form";
 import PackingList from "./PackingList";
 import Stats from "./Stats";
 
-export const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: true },
-  { id: 3, description: "Charger", quantity: 1, packed: false },
-];
-
 function App() {
-  const [items, setItems] = useState(initialItems);
+  const [items, setItems] = useState([]);
 
   const handleAddItem = (item) => {
     setItems((prevItems) => [...prevItems, item]);
@@ -28,6 +22,14 @@ function App() {
     );
   };
 
+  const handleClearList = () => {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete all items?"
+    );
+
+    if (confirmed) setItems([]);
+  };
+
   return (
     <div className="app">
       <h1>🏝️ Far Away 🧳</h1>
@@ -36,8 +38,9 @@ function App() {
         items={items}
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
+        onClearList={handleClearList}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,9 +1,24 @@
 import React from "react";
 
-const Stats = () => {
+const Stats = ({ items }) => {
+  if (!items)
+    return (
+      <footer className="stats">
+        <em>Start adding some items to your packing list 🚀</em>
+      </footer>
+    );
+
+  const totalItems = items.length;
+  const packedItems = items.filter((item) => item.packed === true).length;
+  const percentagePacked = Math.round((packedItems / totalItems) * 100);
+
   return (
     <footer className="stats">
-      <em>👜 You have X items on your lilst, and you already packed X (X%)</em>
+      <em>
+        {percentagePacked === 100
+          ? `You got everything! Ready to go ✈️`
+          : `👜 You have ${totalItems} items on your lilst, and you already packed ${packedItems} (${percentagePacked}%)`}
+      </em>
     </footer>
   );
 };
